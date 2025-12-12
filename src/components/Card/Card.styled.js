6d0dd3ca6d8680b9437a07c1288041c40031fee8
Алексей <efremovalexei02@gmail.com>
2025-12-13 00:51:12 +0500

    Created components

---
+++ src/components/Card/Card.styled.js
@@ -0,0 +1,100 @@
+import styled from "styled-components";
+
+export const SCards = styled.div`
+  width: 100%;
+  display: block;
+  position: relative;
+`;
+export const SCardsItem = styled.div`
+  padding: 5px;
+  animation-name: card-animation;
+  animation-duration: 500ms;
+  animation-timing-function: linear;
+`;
+export const SCardsCard = styled.div`
+  width: 220px;
+  height: 130px;
+  background-color: #ffffff;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: stretch;
+  padding: 15px 13px 19px;
+`;
+export const SCardGroup = styled.div`
+  width: 100%;
+  height: 20px;
+  margin-bottom: 12px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+export const colors = {
+  Copywriting: "#E9D4FF",
+  Research: "#B4FDD1",
+  "Web Design": "#FFE4C2",
+};
+export const SCardTheme = styled.div`
+  width: auto;
+  height: 20px;
+  padding: 5px 14px;
+  border-radius: 18px;
+  background-color: ${({ $cardTopic }) => colors[$cardTopic]};
+  ${({ $cardTopic }) =>
+    $cardTopic === "Copywriting"
+      ? "color: #9A48F1"
+      : $cardTopic === "Research"
+      ? "color: #06B16E"
+      : $cardTopic === "Web Design"
+      ? "color: #FF6D00"
+      : "color: #FFFFFF"};
+`;
+export const SCardThemeP = styled.p`
+  font-size: 10px;
+  font-weight: 600;
+  line-height: 10px;
+`;
+export const SCardBtn = styled.div`
+  width: 24px;
+  height: 24px;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  padding: 2px;
+`;
+export const SCardBtnDIV = styled.div`
+  width: 4px;
+  height: 4px;
+  border-radius: 50%;
+  background-color: #94a6be;
+`;
+export const SCardTitle = styled.h3`
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 18px;
+  color: #000000;
+  margin-bottom: 10px;
+`;
+export const SCardContent = styled.div`
+  height: 64px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: space-between;
+`;
+export const SCardDate = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+`;
+export const SCardDateSVG = styled.svg`
+  width: 13px;
+`;
+export const SCardDateP = styled.p`
+  margin-left: 6px;
+  font-size: 10px;
+  line-height: 13px;
+  color: #94a6be;
+  letter-spacing: 0.2px;
+`;
---
+++ src/components/Header/Header.styled.js
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+
+export const SHeader = styled.header`
+  width: 100%;
+  margin: 0 auto;
+  background-color: #ffffff;
+`;
+export const SHeaderBlock = styled.div`
+  height: 70px;
+  display: flex;
+  flex-wrap: nowrap;
+  align-items: center;
+  justify-content: space-between;
+  position: relative;
+  top: 0;
+  left: 0;
+  padding: 0 10px;
+`;
+export const SHeaderLogo = styled.div``;
+export const SHeaderLogoIMG = styled.img`
+  width: 85px;
+`;
+export const SHeaderNav = styled.nav`
+  max-width: 290px;
+  padding: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+export const SHeaderBtnMainNew = styled.button`
+  width: 178px;
+  height: 30px;
+  border-radius: 4px;
+  background-color: #565eef;
+  color: #ffffff;
+  border: none;
+  font-size: 14px;
+  line-height: 1;
+  font-weight: 500;
+  margin-right: 20px;
+`;
+export const SHeaderBtnMainNewA = styled.a`
+  color: #ffffff;
+`;
+export const SContainer = styled.div`
+  max-width: 1260px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 0 30px;
+`;
